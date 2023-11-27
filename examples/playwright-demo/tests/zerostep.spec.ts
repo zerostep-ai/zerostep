3770c3c526e3698ac73e82c05ba28a53f1d06454
Todd McNeal <todd@reflect.run>
2023-11-27 13:30:58 -0500

    Add Salesforce example test
    This test is the same scenario covered in our most recent
    blog post, and also effectively the same example that's now
    shown on the homepage.

--- examples/playwright-demo/tests/zerostep.spec.ts
+++ examples/playwright-demo/tests/zerostep.spec.ts
@@ -81,3 +81,51 @@ test.describe('Yahoo Finance', () => {
     expect(formattedPrice > 4000).toEqual(true)
   })
 })
+
+// Replace these values with your Salesforce credentials
+const email = 'test@example.com'
+const password = 'passwordhere'
+const hostname = 'realhostnamehere.develop.lightning.force.com'
+
+test.describe('Salesforce', () => {
+  test('create an opportunity', async ({ page }) => {
+    test.skip(email === 'test@example.com', 'Replace placeholder values to run this test');
+
+    await page.goto('https://login.salesforce.com')
+    await ai(`Enter the username ${email}`, { page, test })
+    await ai(`Enter the password ${password}`, { page, test })
+    await page.click('text="Log In"')
+
+    // Only reaches here if we are successfully authenticated
+    await page.waitForSelector('text="Home"')
+
+    // Navigate directly to Sales app
+    await page.goto(`https://${hostname}/lightning/page/home`)
+    await page.waitForSelector('text="Quarterly Performance"')
+
+    await ai('Click on Opportunities link', { page, test })
+    await page.click('text="New"')
+
+    // Wait for 'New Opportunity' form to be displayed
+    await page.waitForSelector('text="New Opportunity"')
+
+    await ai(`Enter '12000' in the Amount field.`, { page, test })
+    await ai('Enter Test in the opportunity name input', { page, test })
+
+    const thirtyDaysFromNow = new Date()
+    thirtyDaysFromNow.setDate(thirtyDaysFromNow.getDate() + 30)
+    const closeDate = thirtyDaysFromNow.toLocaleDateString('en-US', {
+      month: '2-digit',
+      day: '2-digit',
+      year: 'numeric',
+    })
+
+    await ai(`Input ${closeDate} into the Close Date field`, { page, test })
+    await ai('Click on the Stage dropdown', { page, test })
+    await ai('Click on the Needs Analysis option', { page, test })
+    await ai('Click Save', { page, test })
+
+    const result = await ai('What is the current stage of the opportunity?', { page, test })
+    expect(result).toEqual('Needs Analysis')
+  })
+})
